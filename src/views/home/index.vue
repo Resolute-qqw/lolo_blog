deb880f1234fdd9a8829c98f5ec7d59db6e313eb
李浩淇(EX-LIHAOQI001) <1425679428@qq.com>
2020-10-23 14:58:12 +0800

    blog 初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-container">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.home-container {
+  height: 3000px;
+}
+</style>
